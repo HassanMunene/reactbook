8f3b95ff1729fbaba986daed02a8ee06666cd598
HassanMunene <awanzihassan@gmail.com>
2024-01-04 08:39:07 +0300

    react set up

---
+++ js/index.js
@@ -0,0 +1,9 @@
+ReactDOM.render(<h1>Hello Everyone</h1>, document.getElementById("root"));
+ReactDOM.render(
+    <ul>
+        <li>Item 1</li>
+        <li>Item 2</li>
+        <li>Item 3</li>
+    </ul>,
+    document.getElementById('root2')
+);
