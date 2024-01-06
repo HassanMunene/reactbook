78e16f309622d82c1f70daf7b5c4799e613c7df9
HassanMunene <awanzihassan@gmail.com>
2024-01-06 21:05:16 +0300

    components composibitlity

--- js/project1.js
+++ js/project1.js
@@ -1,17 +1,38 @@
+function Header () {
+    return (
+        <header>
+            <nav><img src="img/react-logo.svg" alt="react-logo" height="40px" width="40px" /></nav>
+        </header>
+    );
+}
 
-const mainSection = (
-    <div>
-        <img src="img/react-logo.svg" alt="react-logo" height="40px" width="40px" />
-        <h1>Fun facts about React</h1>
-        <ul>
-            <li>Was first released in 2013</li>
-            <li>Was originally created by Jordan Walke</li>
-            <li>Has well over 100k stars on Github</li>
-            <li>Is maintained by facebook</li>
-            <li>Powers thousands of enterprise app including mobile apps</li>
-        </ul>
-    </div>
-);
+function MainSection () {
+    return (
+        <div>
+            <h1>Reasons I'm excited to learn React</h1>
+            <ol>
+                <li>It is very friendly</li>
+                <li>It does not have a very steep learning curve</li>
+                <li>It is hot in the market</li>
+                <li>It uses javascript</li>
+            </ol>
+        </div>
+    );
+}
+function Footer () {
+    return (
+        <footer>© 2024 Jaanak development. All rights reserved.</footer>
+    )
+}
+function Page () {
+    return (
+        <div>
+            <Header />
+            <MainSection />
+            <Footer />
+        </div>
+    );
+}
 
 const root = ReactDOM.createRoot(document.getElementById('main-section'));
-root.render(mainSection);
+root.render(<Page />);
