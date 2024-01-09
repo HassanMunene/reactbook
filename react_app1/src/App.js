24da9a3b771b5fefb36725ef27f1e26baaee4e2e
HassanMunene <awanzihassan@gmail.com>
2024-01-09 14:53:06 +0300

    still on react boy

--- react_app1/src/App.js
+++ react_app1/src/App.js
@@ -1,21 +1,11 @@
 import './App.css';
 
 function App() {
-  const name = null;
   return (
     <div className="App">
-      <h1>Hello {2 + 2}!</h1>
-
-      {name ? (
-        <>
-          <h1>Hello {name}</h1>
-        </>
-      ): (
-        <>
-          <h1>testing name</h1>
-          <p>There is no name here</p>
-        </>
-      )}
+      <button>-</button>
+      <button>0</button>
+      <button>+</button>
     </div>
   );
 }
