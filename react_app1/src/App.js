4434423430d632ab75cc9e2af1d7d4e6eb086b5a
HassanMunene <awanzihassan@gmail.com>
2024-01-10 17:45:31 +0300

    reconfiguration

--- react_app1/src/App.js
+++ react_app1/src/App.js
@@ -1,6 +1,19 @@
 import React from 'react';
+import { useEffect } from 'react';
+
+// OMDB API KEY  6a1de686
+
+const API_URL = 'http://www.omdbapi.com/?i=tt3896198&apikey=6a1de686'
 
 const App = () => {
+    const searchMovie = async (title) => {
+        const response = await fetch(`${API_URL}&s=${title}`);
+        const data = await response.json();
+        console.log(data.Search)
+    }
+    useEffect(() => {
+        searchMovie('Spiderman')
+    }, [])
     return (
         <h1>Yoo man!</h1>
     );
