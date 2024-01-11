fb6c595379e9de7f2631bc1487fbd29fa08a1699
HassanMunene <awanzihassan@gmail.com>
2024-01-11 14:50:28 +0300

    finally the app is live

--- info-site/src/Header.jsx
+++ info-site/src/Header.jsx
@@ -1,10 +1,11 @@
 import React from 'react';
+import logo from './assets/react.svg'
 
 function Header () {
     return (
         <header className="nav-container">
             <nav className="navbar">
-                <img src="img/react-logo.svg" alt="react-logo" className="nav-logo"/>
+                <img src={logo} alt="react-logo" className="nav-logo"/>
                 <ul className="nav-items">
                     <li className="item">Pricing</li>
                     <li className="item">About</li>
