65bdcbc071fe03df07418678cfeb811e7f10ab39
HassanMunene <awanzihassan@gmail.com>
2024-01-18 21:00:30 +0300

    creating the bone structure

--- react_app2/src/index.js
+++ react_app2/src/index.js
@@ -13,10 +13,16 @@ const Book = () => {
     return (
         <article>
             <Image />
+            <Title />
+            <Author />
         </article>
     )
 }
 const Image = () => <h2>This is the image placeholder</h2>
+const Title = () => <h2>Book title</h2>
+const Author = () => {
+    return <h4>Author</h4>
+}
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(<BookList />)
