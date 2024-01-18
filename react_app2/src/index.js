e74c336d4316237e656303e9fb29028acac5f6b7
HassanMunene <awanzihassan@gmail.com>
2024-01-18 20:55:46 +0300

    still working with functional components

--- react_app2/src/index.js
+++ react_app2/src/index.js
@@ -1,9 +1,23 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 
-const Greeting = () => {
-    return <h2>Hello my first component</h2>
+const BookList = () => {
+    return (
+        <section>
+            <Book />
+        </section>
+    )
 }
 
+const Book = () => {
+    return (
+        <article>
+            <Image />
+        </article>
+    )
+}
+const Image = () => <h2>This is the image placeholder</h2>
+
 const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(<Greeting />)
+root.render(<BookList />)
+
