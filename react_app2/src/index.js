80df29ddac14afd9f40559724ebc01e335a411b3
HassanMunene <awanzihassan@gmail.com>
2024-01-19 11:44:22 +0300

    using the map function

--- react_app2/src/index.js
+++ react_app2/src/index.js
@@ -3,52 +3,77 @@ import ReactDOM from 'react-dom/client';
 
 import './index.css'
 
-const firstBook = {
-    title: 'First Lie Wins',
-    author: 'Ashley Elston',
-    img: 'https://images-na.ssl-images-amazon.com/images/I/810CZ-9akdL._AC_UL600_SR600,400_.jpg',
-    alt: 'First Lie Wins'
-}
-const secondBook = {
-    title: 'Fourth Wing',
-    author: 'Rebecca Yarros',
-    img: 'https://images-na.ssl-images-amazon.com/images/I/91n7p-j5aqL._AC_UL600_SR600,400_.jpg',
-    alt: 'Fourth Wing'
-}
-const thirdBook = {
-    title: 'Iron Flame',
-    author: 'Rebecca Yarros',
-    img: 'https://images-na.ssl-images-amazon.com/images/I/917Bc9C1MlL._AC_UL600_SR600,400_.jpg',
-    alt: 'Iron Flame'
-}
-const fourthBook = {
-    title: 'The Bible in 52 Weeks',
-    author: 'Dr. Kimberly D. Moore',
-    img: 'https://images-na.ssl-images-amazon.com/images/I/913KWawg9gL._AC_UL600_SR600,400_.jpg',
-    alt: 'The Bible in 52 Weeks'
-}
+// const firstBook = {
+//     title: 'First Lie Wins',
+//     author: 'Ashley Elston',
+//     img: 'https://images-na.ssl-images-amazon.com/images/I/810CZ-9akdL._AC_UL600_SR600,400_.jpg',
+//     alt: 'First Lie Wins'
+// }
+// const secondBook = {
+//     title: 'Fourth Wing',
+//     author: 'Rebecca Yarros',
+//     img: 'https://images-na.ssl-images-amazon.com/images/I/91n7p-j5aqL._AC_UL600_SR600,400_.jpg',
+//     alt: 'Fourth Wing'
+// }
+// const thirdBook = {
+//     title: 'Iron Flame',
+//     author: 'Rebecca Yarros',
+//     img: 'https://images-na.ssl-images-amazon.com/images/I/917Bc9C1MlL._AC_UL600_SR600,400_.jpg',
+//     alt: 'Iron Flame'
+// }
+// const fourthBook = {
+//     title: 'The Bible in 52 Weeks',
+//     author: 'Dr. Kimberly D. Moore',
+//     img: 'https://images-na.ssl-images-amazon.com/images/I/913KWawg9gL._AC_UL600_SR600,400_.jpg',
+//     alt: 'The Bible in 52 Weeks'
+// }
 
-
+const booksArray = [
+    {
+        title: 'First Lie Wins',
+        author: 'Ashley Elston',
+        img: 'https://images-na.ssl-images-amazon.com/images/I/810CZ-9akdL._AC_UL600_SR600,400_.jpg',
+        alt: 'First Lie Wins'
+    },
+    {
+        title: 'Fourth Wing',
+        author: 'Rebecca Yarros',
+        img: 'https://images-na.ssl-images-amazon.com/images/I/91n7p-j5aqL._AC_UL600_SR600,400_.jpg',
+        alt: 'Fourth Wing'
+    },
+    {
+        title: 'Iron Flame',
+        author: 'Rebecca Yarros',
+        img: 'https://images-na.ssl-images-amazon.com/images/I/917Bc9C1MlL._AC_UL600_SR600,400_.jpg',
+        alt: 'Iron Flame'
+    },
+    {
+        title: 'The Bible in 52 Weeks',
+        author: 'Dr. Kimberly D. Moore',
+        img: 'https://images-na.ssl-images-amazon.com/images/I/913KWawg9gL._AC_UL600_SR600,400_.jpg',
+        alt: 'The Bible in 52 Weeks'
+    },
+]
 
 
 const BookList = () => {
     return (
         <section className='booklist'>
-            <Book img={firstBook.img} alt={firstBook.alt} title={firstBook.title} author={firstBook.author}/>
-            <Book img={secondBook.img} alt={secondBook.alt} title={secondBook.title} author={secondBook.author}/>
-            <Book img={thirdBook.img} alt={thirdBook.alt} title={thirdBook.title} author={thirdBook.author}/>
-            <Book img={fourthBook.img} alt={fourthBook.alt} title={fourthBook.title} author={fourthBook.author}/>
+            {booksArray.map((book) => {
+                return <Book img={book.img} alt={book.alt} title={book.title} author={book.author}/>
+            })}
         </section>
     )
 }
 
 const Book = (props) => {
     console.log(props)
+    const { img, alt, title, author } = props
     return (
         <article className='book'>
-            <img src={props.img} alt={props.alt}/>
-            <h2>{props.title}</h2>
-            <h4>{props.author}</h4>
+            <img src={img} alt={alt}/>
+            <h2>{title}</h2>
+            <h4>{author}</h4>
         </article>
     )
 }
