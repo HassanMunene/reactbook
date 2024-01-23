b94f57db5ebb6a0dd7222a8a9f21614044b86562
HassanMunene <awanzihassan@gmail.com>
2024-01-23 15:44:24 +0300

    using array map method in array

--- react_app3/src/tutorial/1-useState/useState_array.jsx
+++ react_app3/src/tutorial/1-useState/useState_array.jsx
@@ -1,8 +1,19 @@
+import { data } from "../../data";
+
 const People = () => {
+    const dataArray = data.map((person) => {
+        return (
+            <div className="person">
+                <h3>{person.name}</h3>
+                <button type="button" className="btn">Remove</button>
+            </div>
+        )
+    })
     return (
         <div className="container">
-        <h2>useState Array Example Mate!</h2>
-        <div>Our array will be here</div>
+            <h2>useState Array Example Mate!</h2>
+            <div>Our array will be here</div>
+            <div className="people">{dataArray}</div>
         </div>
     )
 }
