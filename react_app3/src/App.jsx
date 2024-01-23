f1e298151cd1bd044bbef1a373af42d80550af82
HassanMunene <awanzihassan@gmail.com>
2024-01-23 15:02:42 +0300

    react project using useState

---
+++ react_app3/src/App.jsx
@@ -0,0 +1,11 @@
+import ErrorExample from "./tutorial/1-useState/error_example";
+import People from "./tutorial/1-useState/useState_array";
+
+function App() {
+  return (
+    <>
+      <People />
+    </>
+  )
+}
+export default App
---
+++ react_app3/src/tutorial/1-useState/useState_array.jsx
@@ -0,0 +1,9 @@
+const People = () => {
+    return (
+        <div className="container">
+        <h2>useState Array Example Mate!</h2>
+        <div>Our array will be here</div>
+        </div>
+    )
+}
+export default People;
