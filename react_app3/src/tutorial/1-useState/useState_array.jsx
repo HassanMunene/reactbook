eb2622c92f545ea78a7e0ba6ac6fd2887059b272
HassanMunene <awanzihassan@gmail.com>
2024-01-24 09:51:33 +0300

    a better way to handle useState in this particular problem we have

--- react_app3/src/tutorial/1-useState/useState_array.jsx
+++ react_app3/src/tutorial/1-useState/useState_array.jsx
@@ -2,32 +2,62 @@ import { data } from "../../data";
 import { useState } from "react";
 
 const People = () => {
-    const [personVisibilities, setPersonVisibility] = useState(data.map((person) => true))
-    const [peopleVisibility, setPeopleVisibility] = useState(true);
-    //console.log(personVisibility);
+    // const [personVisibilities, setPersonVisibility] = useState(data.map((person) => true))
+    // const [peopleVisibility, setPeopleVisibility] = useState(true);
+    // //console.log(personVisibility);
 
-    const changeDisplay = (personID) => {
-        setPersonVisibility((prevVisibility) => prevVisibility.map((isVisible, index) => index === personID ? !isVisible: isVisible));
-    }
-    const changeAllDisplay = () => {
-        console.log('clear all clicked')
-        setPeopleVisibility(!peopleVisibility);
-    }
+    // const changeDisplay = (personID) => {
+    //     setPersonVisibility((prevVisibility) => prevVisibility.map((isVisible, index) => index === personID ? !isVisible: isVisible));
+    // }
+    // const changeAllDisplay = () => {
+    //     console.log('clear all clicked')
+    //     setPeopleVisibility(!peopleVisibility);
+    // }
 
-    const dataArray = data.map((person, index) => {
-        return (
-            <div className="person" key={person.id} style={{ display: personVisibilities[index] ? 'block':'none'}}>
-                <h3>{person.name}</h3>
-                <button type="button" className="btn" onClick={() => changeDisplay(person.id)}>Remove</button>
-            </div>
-        )
-    })
+    // const dataArray = data.map((person, index) => {
+    //     return (
+    //         <div className="person" key={person.id} style={{ display: personVisibilities[index] ? 'block':'none'}}>
+    //             <h3>{person.name}</h3>
+    //             <button type="button" className="btn" onClick={() => changeDisplay(person.id)}>Remove</button>
+    //         </div>
+    //     )
+    // })
+    // return (
+    //     <div className="container">
+    //         <h2>useState Array Example Mate!</h2>
+    //         <div>Our array will be here</div>
+    //         <div className="people" style={{display: peopleVisibility ? 'block': 'none'}}>{dataArray}</div>
+    //         <button className="btn" onClick={changeAllDisplay}>clear all</button>
+    //     </div>
+    // )
+    const [people, setPeople] = useState(data);
+
+    const removePerson = (id) => {
+        console.log(id);
+        const newPeople = people.filter((person) => {
+            if (person.id !== id) {
+                return person;
+            }
+        })
+        setPeople(newPeople);
+    }
+    const clearAllPeople = () => {
+        return setPeople([]);
+    }
     return (
-        <div className="container">
-            <h2>useState Array Example Mate!</h2>
-            <div>Our array will be here</div>
-            <div className="people" style={{display: peopleVisibility ? 'block': 'none'}}>{dataArray}</div>
-            <button className="btn" onClick={changeAllDisplay}>clear all</button>
+        <div className="people">
+            <div>
+            {people.map((person) => {
+            const {id, name} = person;
+            return (
+                <div key={id} className="person">
+                    <h2>{name}</h2>
+                    <button type="button" className="btn" onClick={() => removePerson(id)}>remove</button>
+                </div>
+            )
+            })}
+            </div>
+            <button type="button" className="btn" onClick={clearAllPeople}>clear All</button>
         </div>
     )
 }
